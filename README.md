58658c9c31fda5b807a39633646157a9116e6220
Blake Embrey <hello@blakeembrey.com>
2016-10-03 16:05:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Typed Jsonld
+
+The type definition for [`jsonld`](https://github.com/digitalbazaar/jsonld.js).
+
+## License
+
+MIT
---
+++ test.ts
@@ -0,0 +1,21 @@
+/// <reference path="bundle.d.ts" />
+
+import * as jsonld from 'jsonld';
+
+jsonld.expand(
+  {
+    '@context': 'http://schema.org/',
+    '@type': 'Person',
+    'name': 'Jane Doe',
+    'jobTitle': 'Professor',
+    'telephone': '(425) 123-4567',
+    'url': 'http://www.janedoe.com'
+  },
+  (_, res) => {
+    console.log(JSON.stringify(res, null, 2));
+
+    return jsonld.compact(res, 'http://schema.org/', (_, res) => {
+      console.log(JSON.stringify(res, null, 2));
+    });
+  }
+);
